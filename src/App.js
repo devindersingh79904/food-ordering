cc4f27ce6c8cac5a915a47e25492241f0b872022
Devinder Singh <devindersingh79904@gmail.com>
2024-01-03 19:11:01 +0530

    react router dom lib added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,40 @@ import ReactDom from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-const AppLayout = () => (
-    <div>
-        <Header />
-        <Body />
-        <Footer />
-    </div>
+import {createBrowserRouter,Outlet,RouterProps, RouterProvider} from 'react-router-dom'
+import About from './components/About';
+import ContactUs from './components/ContactUs';
+import Error from './components/Error';
+const AppLayout = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
     );
+}
 
+const appRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout />,
+        errorElement : <Error />,
+        children:[
+            {
+                path: '/',
+                element: <Body />
+            },
+            {
+            path: '/about',
+            element: <About />
+            },
+            {
+                path: '/contact',
+                element: <ContactUs />
+            }
+    ]
+    }
+]);
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter}  />);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 const Header = () => {
+
+  //state variable are always declared inside a component function never outside 
   const [isLogin, setIsLogin] = useState(false);
+  
+  //if no dependency array is passed, useEffect will be called on every render
+  //if empty dependency array is passed, useEffect will be called only once initial render
+
+  useEffect(() => {
+    console.log("Use Effect Call");
+  }, [isLogin]);
+
+  
+  
   onBtnClick = () => {
     setIsLogin(!isLogin);
    // isLogin = !isLogin;
@@ -14,9 +27,9 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact us</li>
+          <li><Link to="/" >Home</Link></li>
+          <li><Link to="/about" >About Us</Link></li>
+          <li><Link to="/contact" >Contact us</Link></li>
           <li>Cart</li>
           { !isLogin ? (
             <li>
